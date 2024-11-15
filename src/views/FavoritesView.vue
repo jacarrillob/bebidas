d5fc884bea00899776392e8884b8570c1a874ba9
Jose A. Carrillo <josecarrillo1991.jc@gmail.com>
2024-11-15 17:53:46 -0500

    fix: config tsconfig.app.json

--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -16,7 +16,7 @@ const favoritos = useFavoritosStore()
     <div v-else class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
     <RecetaComponent
       v-for="favorito in favoritos.favoritos"
-      :key="favorito?.idDrink"
+      :key="favorito.idDrink"
       :receta="favorito"
     />
   </div>
